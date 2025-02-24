9a3161d8e9d0037d13fad1812a12cc72b9db55d4
pkaho <pkaho@qq.com>
2025-02-24 22:32:47 +0000

    cyberdream colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"scottmckendry/cyberdream.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"mawkler/modicator.nvim",
+			enabled = true,
+		},
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent = false,
+			italic_comments = true,
+			variant = "auto", -- default, auto, light
+		},
+  },
+}
