429815e9b15e1da7fbc71ab3b1ed54eb401450a0
pkaho <pkaho@qq.com>
2025-02-24 22:30:52 +0000

    set gh/gl, jk, <leader>l keymaps

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+vim.g.mapleader = " "
+local map = vim.keymap.set
+
+map("i", "jk", "<ESC>", { desc = "Quit Insert Mode" })
+
+map({ "n", "v", "o" }, "gh", "^", { desc = "To the start of the line" })
+map({ "n", "v", "o" }, "gl", "$", { desc = "To the end of the line" })
+
+map("n", "<leader>l", "<CMD>Lazy<CR>", { desc = "Lazy" })
