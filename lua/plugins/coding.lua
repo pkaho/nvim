29ac6d7097eac10340f2ad9a243d1d4266e54644
pkaho <pkaho@qq.com>
2025-02-24 22:38:59 +0000

    add mini.ai mini.surround plugin

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -4,5 +4,30 @@ return {
     event = {"InsertEnter", "CmdlineEnter"},
     branch = "v0.6",
     opts={}
+  },
+
+  {
+    "echasnovski/mini.ai",
+    version = false,
+    opts = {}
+  },
+
+  {
+    "echasnovski/mini.surround",
+    version = false,
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+
+        suffix_last = "l",
+        suffix_next = "n"
+      }
+    }
   }
 }
