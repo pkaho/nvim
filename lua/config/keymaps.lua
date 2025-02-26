8ab76770ef2263ee3667ebd32aa3aa2e574fc5c2
pkaho <pkaho@qq.com>
2025-02-26 09:37:43 +0000

    add esc unhighlight

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,4 +6,6 @@ map("i", "jk", "<ESC>", { desc = "Quit Insert Mode" })
 map({ "n", "v", "o" }, "gh", "^", { desc = "To the start of the line" })
 map({ "n", "v", "o" }, "gl", "$", { desc = "To the end of the line" })
 
+map("n", "<ESC>", "<CMD>nohlsearch<CR>", { desc = "Unhighlight" })
+
 map("n", "<leader>l", "<CMD>Lazy<CR>", { desc = "Lazy" })
