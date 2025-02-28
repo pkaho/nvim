25f8d42737b9af8f7e5c55f8a2711d885d01d518
pkaho <pkaho@qq.com>
2025-02-28 16:26:09 +0000

    Add back to last edited position

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,3 +5,20 @@ vim.api.nvim_create_autocmd({ "BufEnter", "InsertEnter" }, {
     vim.opt.formatoptions:remove({ "c", "r", "o" })
   end,
 })
+
+-- 记忆最后一次编辑的位置，不同于常规使用 mark 的方式
+-- 利用 view 功能实现记忆上一次编辑位置的功能
+vim.opt.viewoptions:append("cursor")
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  pattern = "*",
+  callback = function()
+    vim.cmd('mkview')
+  end,
+})
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = "*",
+  callback = function()
+    vim.cmd('silent! loadview')
+  end,
+})
+
