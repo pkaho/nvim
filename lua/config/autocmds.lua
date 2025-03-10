df5c108149bcd9a70907b52260f97b50aadc605d
pkaho <pkaho@qq.com>
2025-03-10 16:18:34 +0800

    add some autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,7 +9,7 @@ vim.api.nvim_create_autocmd({ "BufEnter", "InsertEnter" }, {
 -- 记忆最后一次编辑的位置，不同于常规使用 mark 的方式
 -- 利用 view 功能实现记忆上一次编辑位置的功能
 vim.opt.viewoptions:append("cursor")
-vim.api.nvim_create_autocmd("BufWinLeave", {
+vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
     pattern = "*",
     callback = function()
         if vim.bo.buftype == "" then
@@ -17,8 +17,7 @@ vim.api.nvim_create_autocmd("BufWinLeave", {
         end
     end,
 })
-
-vim.api.nvim_create_autocmd("BufWinEnter", {
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
     pattern = "*",
     callback = function()
         if vim.bo.buftype == "" then
@@ -27,3 +26,37 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
     end,
 })
 
+-- 高亮复制行
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+    callback = function()
+        (vim.hl or vim.highlight).on_yank()
+    end,
+})
+
+-- 特定类型文件开启换行和拼写检查
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+    callback = function()
+        vim.opt_local.wrap = true
+        vim.opt_local.spell = true
+    end,
+})
+
+-- 修复 JSON 文件的屏蔽
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = { "json", "jsonc", "json5" },
+    callback = function()
+        vim.opt_local.conceallevel = 0
+    end,
+})
+
+-- 保存文件时自动创建文件夹，以防某些中间文件夹不存在
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    callback = function(event)
+        if event.match:match("^%w%w+:[\\/][\\/]") then
+            return
+        end
+        local file = vim.uv.fs_realpath(event.match) or event.match
+        vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+    end,
+})
