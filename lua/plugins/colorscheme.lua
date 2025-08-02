caf80c5dcfaf9a6354ed14d86db150e57cc28c10
pkaho <pkaho@qq.com>
2025-08-02 23:40:51 +0800

    rose-pine: add colorscheme to prevent theme display errors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -36,9 +36,9 @@ return {
         lazy = false,
         priority = 1000,
         opts = {},
-        -- config = function()
-        --     vim.cmd("colorscheme rose-pine")
-        -- end
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end
     },
 
     -- catppuccin
