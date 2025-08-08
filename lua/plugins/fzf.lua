2f27a9f3fa2c0c5ee2ec819260e19dfdcb6f16ca
pkaho <pkaho@qq.com>
2025-08-08 14:06:35 +0800

    add keymap

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,6 +3,8 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
         { "<leader><space>", "<cmd>lua require('fzf-lua').files()<cr>", desc = "Find Files" },
+        { "<leader>sb", "<cmd>lua require('fzf-lua').buffers()<cr>", desc = "Find Files" },
+        { "<leader>H", "<cmd>lua require('fzf-lua').help_tags()<cr>" },
     },
     opts = {},
 }
