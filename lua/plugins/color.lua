46d6086634407293e01ac51a991f255a88a9c8f5
pkaho <pkaho@qq.com>
2026-09-22 00:14:07 +0800

    refactor: rebind ccc picker to leader uC

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,18 +1,18 @@
--- 颜色工具: 颜色代码高亮 / 交互式取色
 return {
-    -- nvim-colorizer: 高亮文件中的颜色代码 (#hex/rgb/hsl 等)
+    -- nvim-colorizer: 高亮文件中的颜色代码
     {
         "catgoose/nvim-colorizer.lua",
         event = { "BufReadPost", "BufNewFile" }, -- 打开文件时才高亮颜色
         opts = {}
     },
 
-    -- ccc: 交互式颜色选择器 (c 前缀留给 code, 放到 ui 组 <leader>uc)
+    -- ccc: 交互式颜色选择器
+    -- (键位用 <leader>uC: <leader>uc 已被 snacks 的 Conceal Level toggle 占用)
     {
         "uga-rosa/ccc.nvim",
         cmd = "CccPick",
         keys = {
-            { "<leader>uc", "<CMD>CccPick<CR>", desc = "Color Pick (ccc)" }
+            { "<leader>uC", "<CMD>CccPick<CR>", desc = "Color Pick (ccc)" }
         },
         opts = {},
     },
