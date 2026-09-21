d665af668df6d7931db49687cec6bc5cd5876efb
pkaho <pkaho@qq.com>
2026-09-22 00:14:15 +0800

    style: format todo-comments keymaps

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,5 +1,5 @@
--- todo-comments: 高亮并跳转 TODO/FIXME/NOTE 等注释
 return {
+    -- todo-comments: 高亮并跳转 TODO/FIXME/NOTE 等注释
     {
         "folke/todo-comments.nvim",
         event = { "BufReadPost", "BufNewFile" },
@@ -7,12 +7,14 @@ return {
         dependencies = { "nvim-lua/plenary.nvim" },
         opts = {},
         keys = {
-            { "]t",         function() require("todo-comments").jump_next() end,              desc = "Next Todo Comment" },
-            { "[t",         function() require("todo-comments").jump_prev() end,              desc = "Prev Todo Comment" },
-            { "<leader>xt", "<cmd>Trouble todo toggle<cr>",                                   desc = "Todo (Trouble)" },
+            -- stylua: ignore start
+            { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+            { "[t", function() require("todo-comments").jump_prev() end, desc = "Prev Todo Comment" },
+            { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
             { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
-            { "<leader>st", "<cmd>TodoTelescope<cr>",                                         desc = "Todo" },
-            { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>",                 desc = "Todo/Fix/Fixme" },
+            { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+            { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+            -- stylua: ignore end
         },
     },
 }
