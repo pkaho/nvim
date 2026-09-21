ce5f832a967dd048a9e4791f0904717918ffc172
pkaho <pkaho@qq.com>
2026-09-22 00:14:07 +0800

    refactor: rebind themify toggle to leader uB

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,12 @@
 return {
-    -- themify: 主题管理器, <leader>ub 交互式切换多套 colorscheme
+    -- themify: 主题管理器
+    -- (键位用 <leader>uB: <leader>ub 已被 snacks 的 Dark Background toggle 占用)
     {
         "lmantw/themify.nvim",
         lazy = false,   -- 不设置懒加载，懒加载会导致主题不生效
         priority = 999, -- 保持优先级
         keys = {
-            { "<leader>ub", "<CMD>Themify<CR>", desc = "Colorscheme Theme" }
+            { "<leader>uB", "<CMD>Themify<CR>", desc = "Colorscheme Theme" }
         },
         opts = {
             "Shatur/neovim-ayu",
