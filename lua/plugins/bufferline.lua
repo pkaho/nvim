4f23736c31d83ab3239447cf2e983f9761733797
pkaho <pkaho@qq.com>
2026-09-23 00:46:09 +0800

    feat: add buffer pick keymap

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,6 +5,7 @@ return {
         event = "VeryLazy",
         dependencies = "nvim-tree/nvim-web-devicons",
         keys = {
+            -- stylua: ignore start
             { "<leader>bb", "<cmd>e #<CR>",                              desc = "Switch to Other Buffer" },
             { "<leader>b`", "<cmd>e #<CR>",                              desc = "Switch to Other Buffer" },
             { "<leader>bd", function() Snacks.bufdelete() end,           desc = "Delete Buffer" },
@@ -16,30 +17,35 @@ return {
             { "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<CR>",   desc = "Delete Non-Pinned Buffers" },
             { "<leader>br", "<cmd>BufferLineCloseRight<CR>",             desc = "Delete Buffers to the Right" },
             { "<leader>bl", "<cmd>BufferLineCloseLeft<CR>",              desc = "Delete Buffers to the Left" },
+            { "<leader>bj", "<cmd>BufferLinePick<cr>",                   desc = "Pick Buffer" },
             { "<S-h>",      "<cmd>BufferLineCyclePrev<cr>",              desc = "Prev Buffer" },
             { "<S-l>",      "<cmd>BufferLineCycleNext<cr>",              desc = "Next Buffer" },
             { "[b",         "<cmd>BufferLineCyclePrev<cr>",              desc = "Prev Buffer" },
             { "]b",         "<cmd>BufferLineCycleNext<cr>",              desc = "Next Buffer" },
             { "[B",         "<cmd>BufferLineMovePrev<cr>",               desc = "Move buffer prev" },
             { "]B",         "<cmd>BufferLineMoveNext<cr>",               desc = "Move buffer next" },
-            { "<leader>bj", "<cmd>BufferLinePick<cr>",                   desc = "Pick Buffer" },
+            -- stylua: ignore start
         },
         opts = {
             options = {
-                close_command = function(n) require("snacks").bufdelete(n) end,
-                right_mouse_command = function(n) require("snacks").bufdelete(n) end,
+                close_command = function(n)
+                    require("snacks").bufdelete(n)
+                end,
+                right_mouse_command = function(n)
+                    require("snacks").bufdelete(n)
+                end,
                 always_show_bufferline = true,
-                separator_style = 'slope', -- slant | slope | thick | thin | { 'any', 'any' }
+                separator_style = "slope", -- slant | slope | thick | thin | { 'any', 'any' }
                 diagnostics = "nvim_lsp",
                 diagnostics_indicator = function(_, _, diag)
                     local icons = {
                         Error = " ",
-                        Warn  = " ",
-                        Hint  = " ",
-                        Info  = " ",
+                        Warn = " ",
+                        Hint = " ",
+                        Info = " ",
                     }
-                    local ret = (diag.error and icons.Error .. diag.error .. " " or "") ..
-                        (diag.warning and icons.Warn .. diag.warning or "")
+                    local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+                        .. (diag.warning and icons.Warn .. diag.warning or "")
                     return vim.trim(ret)
                 end,
                 offsets = {
@@ -62,7 +68,9 @@ return {
             vim.api.nvim_create_autocmd({ "BufDelete", "BufAdd" }, {
                 callback = function()
                     vim.schedule(function()
-                        pcall(function() vim.cmd.redrawtabline() end)
+                        pcall(function()
+                            vim.cmd.redrawtabline()
+                        end)
                     end)
                 end,
             })
